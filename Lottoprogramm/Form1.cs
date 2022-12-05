c103ebfd5183dcbaa2f83c8ffc3e5dd100f63e78
profiworker2911 <117895189+Noah29112004@users.noreply.github.com>
2022-12-05 10:36:01 +0100

    Bild, Button und Textbox eingefügt

--- Lottoprogramm/Form1.cs
+++ Lottoprogramm/Form1.cs
@@ -16,5 +16,10 @@ namespace Lottoprogramm
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
