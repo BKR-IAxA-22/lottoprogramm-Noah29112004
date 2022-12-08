485b3484141f4fa67a5f0f624b4169d85440f74c
profiworker2911 <117895189+Noah29112004@users.noreply.github.com>
2022-12-08 08:16:29 +0100

    Array befüllen Array überprüfen

--- Lottoprogramm/Form1.cs
+++ Lottoprogramm/Form1.cs
@@ -15,14 +15,34 @@ namespace Lottoprogramm
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             int[] zahlen = new int[6];
             Random r =new Random();
-            for(int i = 0; i < 6; i++)
-            { zahlen[i] = r; }
+
+
+            for(int i = 0; i < zahlen.Length; i++)
+            {
+                int check=r.Next(1,49);
+                if (zahlen.Contains(check))
+                {
+                    i--;
+                }
+                else 
+                {
+                    zahlen[i] = check;
+                }  
+            }
+            textBox1.Text = zahlen[0].ToString();
+            textBox2.Text = zahlen[1].ToString();
+            textBox3.Text = zahlen[2].ToString();
+            textBox4.Text = zahlen[3].ToString();
+            textBox5.Text = zahlen[4].ToString();
+            textBox6.Text = zahlen[5].ToString();   
+           
 
 
         }
